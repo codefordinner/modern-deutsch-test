ce4e4afac9907c81698ee516772d3955a64209d5
codefordinner <harasymchuk987@gmail.com>
2026-09-19 11:19:53 +0200

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Navbar } from './components/Navbar';
 import { Scorebar } from './components/Scorebar';
 import { NumbersTrainer } from './components/NumbersTrainer';
